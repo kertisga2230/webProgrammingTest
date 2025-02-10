0274084c0f5c11ad3867561212843dac77e3df2d
Kertis <itamar.kertis@gmail.com>
2025-02-10 08:59:44 +0200

    Add project files.

---
+++ webProgrammingTest/Pages/mainPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webProgrammingTest.Pages
+{
+    public class mainPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
