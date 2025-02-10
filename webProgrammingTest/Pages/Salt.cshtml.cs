4df28670d9f78406aff3febd00f85e7fa4824776
Kertis <itamar.kertis@gmail.com>
2025-02-10 09:43:24 +0200

    end (before last changes) of website

---
+++ webProgrammingTest/Pages/Salt.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webProgrammingTest.Pages
+{
+    public class SaltModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
